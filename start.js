f8ca5ddcd6a0637520d035da0402ddf9a5fc1470
tofing <denis2001dewe@gmail.com>
2025-05-25 17:12:11 +0700

    Delete start.js

--- start.js
+++
@@ -1,8 +0,0 @@
-async function start() {
-    let characterCardBox = document.querySelector('#character-card-box');
-    let characterModalBox = document.querySelector('#character-modal-box');
-    let characters = await fetchCharacters();
-
-    characterCardBox.innerHTML = getCharacterCards(characters).join('');
-    characterModalBox.innerHTML = getCharacterModals(characters).join('');
-}
